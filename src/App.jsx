76ce7c6bf0d02067ba290230fef192021d53c5ad
kadimakeith <keithkadima@gmail.com>
2023-06-04 17:39:36 +0300

    add some scaffolding stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
 //import { useState } from 'react'
-import Map from './components/map.jsx'
-import MapComponent from './components/testmap.jsx'
-import LandingPage from './components/landingpage.jsx'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Drivers from './components/drivers.jsx';
-import DriverReg from './components/driverReg.jsx'
-import SignUpForm from './components/signup.jsx'
-import SignInForm from './components/signin.jsx'
-import Dashboard from './pages/dashboard.jsx'
-import './App.css'
+import LandingPage from "./components/landingpage.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Staff from "./components/staff.jsx";
+import DriverList from "./components/driverList.jsx";
+import DriverReg from "./components/driverReg.jsx";
+import SignUpForm from "./components/signup.jsx";
+import SignInForm from "./components/signin.jsx";
+import Dashboard from "./pages/dashboard.jsx";
+import "./App.css";
 
-function App() {
 
+const App = () =>  {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route exact path='/' element={<Dashboard/>} />
-        <Route path='/drivers' element={<Drivers/>} />
-        <Route path='/drivers/register' element={<DriverReg/>} />
-        <Route path='' element={<SignUpForm />}/>
-        <Route path='' element={<SignInForm />}/>
-        <Route path='' element={<LandingPage />}/>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          
+            <Route path="/staff" element={<Staff />} />
+            <Route path="/drivers/register" element={<DriverReg />} />
+            <Route path="/drivers/update" element={<DriverList />} />
+            <Route path="/sign" element={<SignInForm />} />
+            <Route path="" element={<SignInForm />} />
+            <Route path="" element={<SignInForm />} />
+            <Route path="" element={<SignInForm />} />
+            <Route path="" element={<SignUpForm />} />
+            <Route path="" element={<SignInForm />} />
+            <Route path="/dashboard" element={<Dashboard/>}/>
+          
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import "leaflet/dist/leaflet.css"; // <- Leaflet styles
-
+import "leaflet/dist/leaflet.css"; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
