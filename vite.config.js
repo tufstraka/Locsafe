8ba95bc0e32faf3b1b5f1f39ca0e4286ed8485c5
tufstraka <keithkadima@gmail.com>
2023-06-10 17:56:21 +0300

    fix for relative references

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     rollupOptions: {
-      external: ["leaflet"],
+      external: ["/leaflet"],
       output: {
         globals: {
           leaflet: "L",
