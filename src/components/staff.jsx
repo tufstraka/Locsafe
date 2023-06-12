b5826f0c65fa45e16093d0e61a9a68f090cf0fa9
kadimakeith <keithkadima@gmail.com>
2023-06-12 13:24:06 +0300

    chore: update routes

--- src/components/staff.jsx
+++ src/components/staff.jsx
@@ -5,7 +5,7 @@ const Staff = () => {
   return (
     <>
     
-    <div className="flex gap-3">
+    <div className="flex gap-3 mr-3">
     <Sidebar/>  
       
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -39,9 +39,11 @@ const Staff = () => {
             Click here to update the details of an existing driver in the GPS
             fleet tracking system.
           </p>
+          <a href='/drivers/update'>
           <button className="mt-4 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded">
             Update
           </button>
+          </a>
         </div>
     
 
@@ -56,7 +58,7 @@ const Staff = () => {
             Click here to register a new dispatcher in the GPS fleet tracking
             system.
           </p>
-          <a href="/drivers/register">
+          <a href="/dispatchers/register">
             <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
               Register
             </button>
@@ -71,10 +73,10 @@ const Staff = () => {
             <h2 className="text-xl font-bold">Update Dispatcher Details</h2>
           </div>
           <p className="text-gray-600">
-            Click here to update the details of an existing driver in the GPS
+            Click here to update the details of an existing dispatcher in the GPS
             fleet tracking system.
           </p>
-          <a href='/drivers/update'>
+          <a href='/dispatchers/update'>
           <button className="mt-4 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded">
             Update
           </button>
