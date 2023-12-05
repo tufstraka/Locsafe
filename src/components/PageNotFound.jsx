79564a68ebfee4a0f116e99762f55b40e83e6488
tufstraka <keithkadima@gmail.com>
2023-12-05 13:22:52 +0300

    feat: add 404 error page

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,10 @@
+const PageNotFound = () => {
+  return (
+    <div>
+      <h2>404 - Not Found</h2>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
