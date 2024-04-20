872688a53a5ba347a5b1a856b05c040c63756a97
kadimakeith <keithkadima@gmail.com>
2024-04-20 11:18:27 +0000

    style: use more contrast

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,23 +1,26 @@
+import React from 'react';
+
 const Footer = () => (
-    <footer className="py-6 bg-white shadow-lg">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
-        <p className="text-gray-600 text-sm md:text-base text-center md:text-left mb-4 md:mb-0">© 2024 Locsafe. All rights reserved.</p>
-        <nav>
-          <ul className="flex items-center space-x-4 md:space-x-8 text-gray-600 font-medium justify-center">
-            <li>
-              <a href="/Terms-and-Conditions" className="hover:text-gray-800">Terms</a>
-            </li>
-            <li>
-              <a href="/Privacy-Policy" className="hover:text-gray-800">Privacy</a>
-            </li>
-            <li>
-              <a href="/Support" className="hover:text-gray-800">Support</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </footer>
-  );
-  
-  export default Footer;
+  <footer className="py-6 bg-gray-900 shadow-lg">
+    <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
+      <p className="text-white text-sm md:text-base text-center md:text-left mb-4 md:mb-0">© 2024 Locsafe. All rights reserved.</p>
+      <nav>
+        <ul className="flex items-center space-x-4 md:space-x-8 text-white font-medium justify-center">
+          <li>
+            <a href="/Terms-and-Conditions" className="hover:text-gray-200">Terms</a>
+          </li>
+          <li>
+            <a href="/Privacy-Policy" className="hover:text-gray-200">Privacy</a>
+          </li>
+          <li>
+            <a href="/Support" className="hover:text-gray-200">Support</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+);
+
+export default Footer;
+
   
