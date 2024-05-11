1141871e52461d0e30f2fe21666aa061cecc5511
kadimakeith <keithkadima@gmail.com>
2024-05-11 14:16:45 +0000

    fix routes

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -6,13 +6,13 @@ import Map from '../components/map.jsx';
 import Sidebar from '../components/sidebar.jsx';
 
 const Dashboard = () => {
-  const [drivers, setDrivers] = useState(0);
+  const [users, setusers] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       const querySnapshot = await getDocs(collection(db, 'drivers'));
       console.log(querySnapshot);
-      setDrivers(querySnapshot.docs.length);
+      setusers(querySnapshot.docs.length);
     };
     fetchData();
   }, []);
@@ -49,7 +49,7 @@ const Dashboard = () => {
                 {/* Cards */}
                 {[
                   { title: 'Total Vehicles', value: '0' },
-                  { title: 'Total Drivers', value: drivers },
+                  { title: 'Total users', value: users },
                   { title: 'Total Alerts', value: '0' },
                   { title: 'Total Trips', value: '0' },
                 ].map((card, index) => (
