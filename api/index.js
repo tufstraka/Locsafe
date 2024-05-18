fc971cd45e8f4d1203263fc9b7f8a5dbaf924dcd
kadimakeith <keithkadima@gmail.com>
2024-05-18 09:26:48 +0000

    ☠

--- api/index.js
+++ api/index.js
@@ -3,12 +3,12 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`app listening at ${port}`);
 });
 
 export default app;
