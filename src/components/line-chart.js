64ce7f41a5010c9da94d519f6105da47b4b0bf7f
kadimakeith <keithkadima@gmail.com>
2024-09-07 14:07:05 +0000

    fix bugs

--- src/components/line-chart.js
+++ src/components/line-chart.js
@@ -1,5 +1,5 @@
 import { Line } from "react-chartjs-2";
-//import { Chart as ChartJS } from "chart.js/auto";
+import { Chart as ChartJS } from "chart.js/auto";
 
 const LineChart = ({chartData}) => {
   return (
